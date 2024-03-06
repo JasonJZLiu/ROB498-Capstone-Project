3a6f191136ce4befe467bdf4d0d23604642c47f9
Jason Liu <jason_liu1116@hotmail.com>
2024-03-06 13:15:35 -0500

    added env set bash scripts

---
+++ bash_scripts/setup_jetson_myhal.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# ----- Connect to TP-LINK_ROB498 -----
+SSID=TP-Link_ROB498
+
+# Check if the WiFi network is already known
+if nmcli con show | grep -q "$SSID"; then
+    echo "WiFi network $SSID is already known. Attempting to connect..."
+    nmcli con up id "$SSID"
+else
+    echo "Adding new WiFi network $SSID."
+    # Add the WiFi network and connect
+    nmcli dev wifi connect "$SSID"
+fi
+
+if [ $? -eq 0 ]; then
+    echo "Successfully connected to $SSID."
+else
+    echo "Failed to connect to $SSID."
+fi
+
+
+# ----- Set up the ROS_IP -----
+
+# Specify the network interface, e.g., eth0, wlan0
+NET_INTERFACE="wlan0"
+
+# Get the IP address of the specified interface
+IP_ADDR=$(hostname -I | awk '{print $1}')
+
+# Export the IP address as ROS_IP
+export ROS_IP=$IP_ADDR
+
+# Update or add ROS_IP in ~/.bashrc
+if grep -q "export ROS_IP=" ~/.bashrc; then
+    # If ROS_IP exists in .bashrc, update it
+    sed -i "/export ROS_IP=/c\export ROS_IP=$ROS_IP" ~/.bashrc
+else
+    # Otherwise, add it to .bashrc
+    echo "export ROS_IP=$ROS_IP" >> ~/.bashrc
+fi
+
+# Print the ROS_IP for confirmation
+echo "ROS_IP is set to $ROS_IP"
+
+
+
+# ----- Set up ROS Master to the ground station -----
+
+# The first command-line argument is the ROS master's IP address
+ROS_MASTER_IP=10.42.0.100
+
+# Set the ROS_MASTER_URI environment variable
+export ROS_MASTER_URI=http://$ROS_MASTER_IP:11311
+
+# Update or add ROS_MASTER_URI in ~/.bashrc
+if grep -q "export ROS_MASTER_URI=" ~/.bashrc; then
+    # If ROS_MASTER_URI exists in .bashrc, update it
+    sed -i "/export ROS_MASTER_URI=/c\export ROS_MASTER_URI=$ROS_MASTER_URI" ~/.bashrc
+else
+    # Otherwise, add it to .bashrc
+    echo "export ROS_MASTER_URI=$ROS_MASTER_URI" >> ~/.bashrc
+fi
+
+# Print the ROS_MASTER_URI for confirmation
+echo "ROS_MASTER_URI is set to $ROS_MASTER_URI"
